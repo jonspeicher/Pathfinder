2ae60b8bec56b5b1cc33a622bd95746907cf8267
Jon Speicher <jon.speicher@gmail.com>
2010-10-31 14:22:30 -0400

    Add header to filename lowercasing helper script

--- helpers/lcfilenames.py
+++ helpers/lcfilenames.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 
+# lcfilenames.py - Makes every file in the current directory have a lower-case filename
+#
+# Syntax:
+#
+#   lcfilenames.py
+
+# Copyright (c) 2010 Jonathan Speicher (jon.speicher@gmail.com)
+# Licensed under the MIT license: http://creativecommons.org/licenses/MIT
+
 import glob, os
 
 for filename in glob.glob('*'):
