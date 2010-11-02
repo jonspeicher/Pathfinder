a7cf7dcbe0b08e0c763ce090f910b9c6614cff27
Jon Speicher <jon.speicher@gmail.com>
2010-11-02 01:01:32 -0400

    Clean up dezulu.py a bit

--- helpers/dezulu.py
+++ helpers/dezulu.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# dezulu.py - Finds all 24-hour time referrences in a file and offsets them by a supplied value
+# dezulu.py - Finds all 24-hour time references in stdin and offsets them by a supplied value
 #
 # Syntax:
 #
@@ -13,14 +13,10 @@
 # Copyright (c) 2010 Jonathan Speicher (jon.speicher@gmail.com)
 # Licensed under the MIT license: http://creativecommons.org/licenses/MIT
 
-import functools, re, sys
-
-def compute_offset(match, offset):
-    return str(int(match.group(1)) + offset) + ':'
+import re, sys
     
 def dezulu(line, offset):
-    substitutor = functools.partial(compute_offset, offset = offset)
-    return re.sub('(\d\d):', substitutor, line).rstrip()
+    return re.sub('(\d\d):', lambda m: str(int(m.group(1)) + offset) + ':', line).rstrip()
 
 for line in sys.stdin:
     print(dezulu(line, int(sys.argv[1])))
