643784993c70688b3ac02689b37fd7b6213c7781
Jon Speicher <jon.speicher@gmail.com>
2010-11-02 00:15:57 -0400

    Make exif2csv.py write to stdout insteead of a file

--- helpers/exif2csv.py
+++ helpers/exif2csv.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 
-# exif2csv.py - Build a CSV file containing filenames and EXIF data for all JPG files in a directory
+# exif2csv.py - Build CSV data containing filenames and EXIF data for all JPG files in a directory
 #
 # Syntax:
 #
-#   exif2csv.py <csv> <tag>
+#   exif2csv.py <tag>
 #
 # Where:
 #
-#   <csv> is the name of the CSV file to write
 #   <tag> is the name of the EXIF tag to read from each JPG file in the directory
 #
 # Requires EXIF.py by Gene Cash and Ianare Sevi (http://sourceforge.net/projects/exif-py)
@@ -19,14 +18,12 @@
 import EXIF, glob, sys
 
 class CsvWriter(object):
-    """Writes a list of tuples to a file, one comma-separated tuple per line"""
-    def __init__(self, filename):
-        self.__filename = filename
+    """Writes a list of tuples to standard output, one comma-separated tuple per line"""
     def append(self, tuples):
-        with open(self.__filename, 'a') as csvfile:
-            csvfile.writelines(self._join_tuples(tuples))
+        for tuple in self._join_tuples(tuples):
+            print(tuple)
     def _join_tuples(self, tuples):
-        return [','.join(tuple) + '\n' for tuple in tuples]
+        return [','.join(tuple) for tuple in tuples]
 
 class ExifReader(object):
     """Reads specified EXIF tags from a specified file"""
@@ -48,7 +45,7 @@ class ExifBuilder(object):
         
 reader = ExifReader()
 builder = ExifBuilder('*.jpg', reader)
-writer = CsvWriter(sys.argv[1])
+writer = CsvWriter()
 
-exif = builder.build(sys.argv[2])
+exif = builder.build(sys.argv[1])
 writer.append(exif)
